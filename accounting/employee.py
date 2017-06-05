f8d617fa2d4ea84050d2fecbd9789529422e7087
waltoncb <walton.cb@gmail.com>
2017-06-05 13:02:52 -0400

    Added HourlyEmployee, SalariedEmployee subclasses

--- accounting/employee.py
+++ accounting/employee.py
@@ -6,4 +6,15 @@ class Employee:
         self.__weekly_dues = weekly_dues;
 
     def get_full_name(self):
-        return "%s, %s" % (self.__last_name, self.__first_name);
+        return "%s, %s" % (self.__last_name, self.__first_name)
+    
+    
+class HourlyEmployee(Employee):
+    def __init__(self, employee_id, first_name, last_name, weekly_dues, hourly_rate):
+        self.__hourly_rate = hourly_rate
+
+
+class SalariedEmployee(Employee):
+    def __init__(self, employee_id, first_name, last_name, weekly_dues, salary, commission_rate):
+        self.__salary = salary
+        self.__commission_rate = commission_rate
