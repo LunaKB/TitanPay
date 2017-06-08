813fb70a591e0bfb26a8921c8f084aef3b2c5a11
waltoncb <walton.cb@gmail.com>
2017-06-08 09:03:09 -0400

    fixed salaried employees call to superclass

--- accounting/employee.py
+++ accounting/employee.py
@@ -17,6 +17,6 @@ class HourlyEmployee(Employee):
 
 class SalariedEmployee(Employee):
     def __init__(self, employee_id, first_name, last_name, weekly_dues, salary, commission_rate):
-        Employee.__init__(self, employee_id, first_name, last_name, salary, commission_rate)
+        Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
         self.__salary = salary
         self.__commission_rate = commission_rate
