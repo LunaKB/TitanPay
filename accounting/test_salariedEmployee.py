e82c520f587031722b7bdc3dd074f4cffde208b6
waltoncb <walton.cb@gmail.com>
2017-07-06 17:29:05 -0400

    Create test_salariedEmployee.py

---
+++ accounting/test_salariedEmployee.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from accounting.employee import SalariedEmployee
+from datetime import datetime
+
+
+class TestSalariedEmployee(TestCase):
+    def test_make_sale(self):
+        test_employee = SalariedEmployee('', '', '', '', '', '', '')
+        test_employee.make_sale(1)
+        self.assertEquals(1, test_employee.get_number_of_receipts())
+
+    def test_calculate_pay(self):
+        test_employee = SalariedEmployee('', '', '', '', '', '','')
+        test_employee.calculate_pay(datetime.now().date(), datetime.now().date())
+        self.assertEquals(0, test_employee.calculate_pay(datetime.now().date(), datetime.now().date()))
